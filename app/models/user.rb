68b9fcf32f980fbf2691e9e1791d62cd3f3441f4
Monatric <nawpnawp8@gmail.com>
2025-07-05 15:39:49 +0800

    feat: associate user with posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :name, presence: true, length: { in: 3..20 }, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
+
+  has_many :posts
 end
