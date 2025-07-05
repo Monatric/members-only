9ff0a8c9adc78f14656f8328bc71e208d4dbf6e7
Monatric <nawpnawp8@gmail.com>
2025-07-05 16:14:43 +0800

    feat: build form submission for Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,28 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create ]
 
-  def new
-  end
-
   def index
   end
 
-  def create
+  def new
+    @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id if current_user
+
+    if @post.save
+      redirect_to root_url, notice: "Post saved successfully!"
+    else
+      flash.now[:error] = "Post cannot be saved."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def post_params
+      params.expect(post: [ :title, :body ])
+    end
 end
